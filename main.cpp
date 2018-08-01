d75aa52e61950648c2bc3f50aa47e1023ce0c240
jtmedley <40803861+jtmedley@users.noreply.github.com>
2018-08-01 18:43:14 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Medley
+ *
+ * Created on July 31, 2018, 4:50 PM
+ */
+
+#include "SerialPort.h"
+#include "SerialPortConstants.h"
+#include "SerialStream.h"
+#include "SerialStreamBuf.h"
+#include "StreamLocation.h"
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
